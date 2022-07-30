eb7816c5bb7fc0e1aac39c617682462b2c5f99df
fawl_03 <razidias@gmail.com>
2022-07-29 23:16:45 -0300

    att 1.4.0 corrigindo nome do arquivo header para Header

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
+// import planets from '../data/planets';
 
 const headlineText = 'Planetas';
 
-class SolarSystem extends React.Component {
+class SolarSystem extends Component {
   render() {
     return (
       <>
         <div data-testid="solar-system" />
         <Title headline={ headlineText } />
+        <PlanetCard />
       </>
     );
   }
